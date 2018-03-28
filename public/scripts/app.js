6d431684285802efe28f6878953bb4f39ac11c54
Sam Schantz <samvschantz@gmail.com>
2018-03-28 20:17:11 +0000

    Dynamic Tweets added

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,45 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+$(function(){
 
+  function createTweetElement(tweetData){
+    var name = tweetData['user']['name'];
+    var largeAvatar = tweetData['user']['avatars']['large'];
+    var handle = tweetData['user']['handle'];
+    var content = tweetData['content']['text'];
+    var created_at = tweetData['created_at'];
+
+    var $tweet = $("<article>").addClass("oldTweet");
+
+    //header
+    var $img = $("<img>").attr("src", largeAvatar);
+    var $displayName = $(`<div>`).addClass("displayName").append(`${name}</div>`);
+    var $userName = $(`<div>`).addClass("userName").append(`${handle}</div>`);
+    var $header = $("<header>");
+    $($header).append($img);
+    $($header).append($displayName);
+    $($header).append($userName);
+
+    $($tweet).append($header);
+
+    //content
+    var $content = $(`<div>${content}</div>`).addClass("content");
+    $($tweet).append($content);
+
+    //footer
+    var $footer = $("<footer>");
+    var $daysSince = $(`<div>${created_at}</div>`).addClass("daysSince");
+    $($footer).append($daysSince);
+    $($tweet).append($footer)
+    $($tweet).append("</article>")
+
+    return $tweet
+  }
+
+  function renderTweets(tweets) {
+    for (i in tweets){
+      var $tweet = createTweetElement(tweets[i])
+      $('#tweetContainer').append($tweet);
+    }
+  }
+
+  renderTweets(data);
+})
