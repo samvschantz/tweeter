768c1a90febbc662aa5038e3021bf0fd17562e7c
Sam Schantz <samvschantz@gmail.com>
2018-03-29 21:38:35 +0000

    getTweets now uses mongoDB

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -9,6 +9,8 @@ module.exports = function makeDataHelpers(db) {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
+      db.collection("tweets")
+
       simulateDelay(() => {
         db.tweets.push(newTweet);
         callback(null, true);
@@ -17,11 +19,12 @@ module.exports = function makeDataHelpers(db) {
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
+      db.collection("tweets").find().toArray((err, tweets) => {
+        if (err) {
+          return callback(err);
+        }
+        callback(null, tweets);
       });
     }
-
-  };
-}
+  }
+};
